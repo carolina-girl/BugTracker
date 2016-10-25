a3cb9e0c2ac05cc0bec51bdf0aad03713dfa1289
Mary Ann Burns <maburns@carolina.rr.com>
2016-10-25 11:25:59 -0400

    initial commit

---
+++ BugTracker/Models/Projects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models
+{
+    public class Projects
+    {
+            public Project()
+            {
+                this.Users = new HashSet();
+            }
+
+            public virtual ICollection Users { get; set; }
+        }
+
+    }
+}
